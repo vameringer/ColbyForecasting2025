87ae2cae8daa591da276d53ce6612a6ce10d121d
Victoria Meringer <vameri25@colby.edu>
2025-01-17 15:57:11 -0500

    Comment out for my model_input

---
+++ functions/model_input.R
@@ -0,0 +1,11 @@
+read_model_input = function(scientificname = "Phocoena phocoena",
+                            mon = "Jan",
+                            approach = "greedy",
+                            path = data_path("model_input")){
+x = read_sf(files[1])}
+
+read_model_input()
+
+
+
+
